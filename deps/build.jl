fa23876082c9baa87a94ea5ee102ba4cf13d4763
David MacMahon <davidm@astro.berkeley.edu>
2023-04-17 07:56:56 +0000

    Add pip version specifiers in build/deps.jl
    This installs the versions of packages for CASA 6.5.2, though version
    2023.4.10 of `casadata` is installed to get the latest data (as of this
    change).

--- deps/build.jl
+++ deps/build.jl
@@ -1,12 +1,12 @@
 using Conda
 Conda.pip_interop(true)
 Conda.pip("install", [
-          "casadata",
-          "casaplotms",
-          "casaplotserver",
-          "casashell",
-          "casatasks",
-          "casatools",
-          "casaviewer",
+          "casadata==2023.4.10",
+          "casaplotms==1.8.7",
+          "casaplotserver==1.4.6",
+          "casashell==6.5.2.26",
+          "casatasks==6.5.2.26",
+          "casatools==6.5.2.26",
+          "casaviewer==1.6.6",
           "python-casacore"
          ])
